946a8fa31b5ceca81f65e7632bcf6a74c9f92c0f
mustabbas <42379733+mustabbas@users.noreply.github.com>
2021-10-06 22:42:18 +0300

    Hit the api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import './styles.css';
+import { addScore, getData, createTable } from './score.js';
+
+const add = document.querySelector('.add');
+const user = document.querySelector('.name');
+const score = document.querySelector('.scoreValue');
+const refresh = document.querySelector('.refresh');
+
+add.addEventListener('click', () => {
+  addScore(user.value, score.value);
+});
+
+refresh.addEventListener('click', () => {
+  getData();
+  createTable();
+});
+
+window.addEventListener('load', () => {
+  createTable();
+});
